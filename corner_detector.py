bf2f62f9b8f132e3fc16fb2776c9061ecaaee360
Blackhole137 <71172644+Blackhole137@users.noreply.github.com>
2020-10-30 09:59:06 +0100

    Update and rename corner detector.py to corner_detector.py
    Changing to module,  file name change to avoid a syntax error.

---
+++ corner_detector.py
@@ -0,0 +1,31 @@
+import sys
+import cv2
+import numpy as np
+class Unknown_algorythm_error(Exception):
+    def __init__(self):
+        pass
+class Corner_detector():
+    def __init__(self, image, detectortype="Harris", corners=[]):
+        self.corners = corners
+        self.image = image
+        self.detectortype = detectortype
+    def update(self, image=None):
+        try:
+            if image.type() != None:
+                self.image = image
+        except:
+            self.image = image
+        gray = np.float32(cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY))
+        if self.detectortype == "Harris":
+            self.corners = cv2.cornerHarris(gray, 3, 3, 0, 1)
+        elif self.detectortype == "Shi-Tomasi":
+            self.corners = cv2.goodFeaturesToTrack(gray, 3, 3, 0, 1)
+        else:
+            raise Unknown_algoryth_error
+        return self.corners
+    def updateanddisplay(self):
+        dst = self.update(image=self.image)
+        self.image[dst>0.01*dst.max()] = [0, 0, 255]
+        return self.image
+
+
