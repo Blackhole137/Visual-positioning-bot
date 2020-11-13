efb2392b5e577e5f89b52e6ac51579a7d0095937
Blackhole137 <71172644+Blackhole137@users.noreply.github.com>
2020-11-13 10:30:09 +0100

    Update alloverlay.py

--- alloverlay.py
+++ alloverlay.py
@@ -2,7 +2,8 @@ import sys
 import cv2
 import numpy as np
 import corner_detector as corn
-videoname = input("enter input")
+#videoname = input("enter input")
+videoname = 0
 try:
 	videoname = int(videoname)
 	cap = cv2.VideoCapture(videoname)
@@ -13,15 +14,15 @@ if cap.isOpened():
     corn1 = corn.Corner_detector(image)
 while cap.isOpened():
     ret, image = cap.read()
-    if ret == True:
-        corn1.image = image
-        dst = corn1.update()
-        #dst = cv2.dilate(dst, None) 
+    if ret:
+        corn1.image = image 
         cv2.imshow("image", corn1.updateanddisplay())
+        print(ret, image)
+        #cv2.imshow("test", image)
         cv2.waitKey(1)
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
     else:
+        print("capture not reading")
         break
 cap.release()
-
