d3b2d27ba91f0b034530f6c73c80bca4e665ac54
Blackhole137 <71172644+Blackhole137@users.noreply.github.com>
2021-03-29 15:16:36 +0200

    Update alloverlay.py
    Apart of fixing some minor errors and implementating the image undistortion system, i have greatly improved the efficiency of the algorythm, as now the data get's streamed through just one "cali" and one "corn" object instead of two of every type. This happens because both cameras are identical, so different calibration or processign parameters are completely unnecessary, and in fact a possible issue. I remain uncertain about the way of implementing the feature matcher, although it would work more coherently with this architecture because of the fact that it would require streaming both images into the same object.

--- alloverlay.py
+++ alloverlay.py
@@ -4,35 +4,50 @@ import numpy as np
 import features as corn
 import camera as cali
 #videoname = input("enter input")
-videoname = 0
+videoname = "camera10001-0200.mkv"
 try:
 	videoname = int(videoname)
 	cap = cv2.VideoCapture(videoname)
 except:
 	cap = cv2.VideoCapture(videoname)
-videoname2 = 0
+videoname2 = "camera 20000-0200.mkv"
 try:
 	videoname = int(videoname)
 	cap2 = cv2.VideoCapture(videoname)
 except:
 	cap2 = cv2.VideoCapture(videoname)
 if cap.isOpened()and cap2.isOpened():
-    ret = []
-    image = []
-    corns = []
-    calis = []
-    for j in range(2):
-        ret1, image1 = cap.read()
-        ret.append(ret1)
-        image.append(np.float32(cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)))
-        corns.append(corn.Corner_detector(image))
-        calis.append(cali.Calibrator(image))
-while cap.isOpened():
+    ret1, image1 = cap.read()
+    ret2, image2 = cap2.read()
+    ret = [ret1, ret2]
+    image = [np.float32(cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)), np.float32(cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY))]
+    cali1 = cali.Calibrator()
+    corn1 = corn.Corner_detector(image)
+while cap.isOpened() and cap2.isOpened():
     ret[0], image[0] = cap.read()
     ret[1], image[1] = cap2.read()
     if ret:
         backupimg = image
         for i, img in enumerate(image):
+            if cali1.calibrated:
+                backupimg[i] = corn1.image = cali1.undistort(np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY)), cali1.mtx, cali1.dist)
+            else:
+                backupimg[i] = corn1.image = np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY))
+            cv2.imshow("camera "+str(i), corn1.updateanddisplay())
+        image = backupimg
+        print(ret, image)
+        #cv2.imshow("test", image)
+        key = cv2.waitKey(1)
+        if key == ord("c"):
+            cali1.calibrate(cali1.image)
+        if cv2.waitKey(25) & 0xFF == ord("q"):
+            break
+    else:
+        print("capture not reading")
+        break
+cap.release()
+
+
             backupimg[i] = np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY))
         image = backupimg
         backupcorns = corns
