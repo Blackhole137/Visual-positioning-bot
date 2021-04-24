701cc91ec786f0d1b636ab7ca6acd59bc7eabcf5
Blackhole137 <71172644+Blackhole137@users.noreply.github.com>
2021-04-24 11:33:35 +0200

    Update alloverlay.py
    changed to orb

--- alloverlay.py
+++ alloverlay.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import features as corn
 import camera as cali
+cv2.ocl.setUseOpenCL(False)
 #videoname = input("enter input")
 videoname = "camera10001-0200.mkv"
 try:
@@ -26,14 +27,17 @@ if cap.isOpened()and cap2.isOpened():
 while cap.isOpened() and cap2.isOpened():
     ret[0], image[0] = cap.read()
     ret[1], image[1] = cap2.read()
+    tomatch = [[], []]
     if ret:
         backupimg = image
         for i, img in enumerate(image):
             if cali1.calibrated:
                 backupimg[i] = corn1.image = cali1.undistort(np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY)), cali1.mtx, cali1.dist)
             else:
-                backupimg[i] = corn1.image = np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY))
-            cv2.imshow("camera "+str(i), corn1.updateanddisplay())
+                backupimg[i] = corn1.image = np.float32(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+            keypoints=corn1.updateanddisplay()[1]
+            tomatch[i].append(keypoints)
+            cv2.imshow("camera "+str(i), cv2.drawKeypoints(img, keypoints, None, color=(0,255,0)))
         image = backupimg
         print(ret, image)
         #cv2.imshow("test", image)
@@ -46,22 +50,3 @@ while cap.isOpened() and cap2.isOpened():
         print("capture not reading")
         break
 cap.release()
-
-
-            backupimg[i] = np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY))
-        image = backupimg
-        backupcorns = corns
-        for i, c in enumerate(corns):
-            backupcorns[i].image = image[i]
-            calis[i].image = image[i]
-            cv2.imshow("image {}".format(i), backupcorns[i].updateanddisplay())
-        corns = backupcorns
-        print(ret, image)
-        #cv2.imshow("test", image)
-        key = cv2.waitKey(1)
-        if key == ord("c"):
-            cali1.calibrate(cali1.image)
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            break
-    else:
-        print("capture not reading")
