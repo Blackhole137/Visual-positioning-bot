183073ad3d986675123d0302bd9045b009ae137d
Blackhole137 <71172644+Blackhole137@users.noreply.github.com>
2022-05-27 10:32:35 +0200

    Update alloverlay.py
    -Avoided issue where frame 200 returns an exception
    -Now main file correctly feeds the keypoint locations to the point position calculation function
    -Now pairs are filtered before calculating relative position to avoid the algorithm attempting to find points that are too distant for the system's baseline and resolution
    -Relative point coordinates are saved and outputted
    Be aware that the next commit to the other two files also belongs to this version, since i don't have time to write three comments at the moment.

--- alloverlay.py
+++ alloverlay.py
@@ -5,11 +5,18 @@ import numpy as np
 import features as corn
 import camera as cali
 cv2.ocl.setUseOpenCL(False)
-#videoname = input("enter input")
+#videoname = "0"
 videoname = "camera10001-0200.mkv"
 cv2.setUseOptimized(True)
 fps = 0
 BLACK = (0,0,0)
+class Loc_importer():
+    def __init__(self):
+        locs = []
+        rots = []
+    def load(self):
+        file = open("cameralocation.txt")
+        
 def frameshower(image):
     global fps, BLACK
     return cv2.putText(image, str(fps), (0,100), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, BLACK, 2)
@@ -18,6 +25,7 @@ try:
 	cap = cv2.VideoCapture(videoname)
 except:
 	cap = cv2.VideoCapture(videoname)
+#videoname2 = "0"
 videoname2 = "camera 20000-0200.mkv"
 try:
 	videoname = int(videoname)
@@ -29,38 +37,44 @@ if cap.isOpened()and cap2.isOpened():
     ret2, image2 = cap2.read()
     ret = [ret1, ret2]
     image = [np.float32(cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)), np.float32(cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY))]
-    cali1 = cali.Calibrator(camera_data={"pixelsize":None, "matrixsize":None, "baseline":1, "lens_distance":None})
+    cali1 = cali.Calibrator(camera_data={"pixelsize":0.02, "matrixsize":(1920, 1080), "baseline":1, "lens_distance":39})
     corn1 = corn.Corner_detector(image)
     matc1 = corn.Feature_matcher()
+point_positions = []
+framecounter = 0
 while cap.isOpened() and cap2.isOpened():
     framestart_time = time.time()
     ret[0], image[0] = cap.read()
     ret[1], image[1] = cap2.read()
-    tomatch = [[[], []], [[], []]]
+    tomatch = [[], []]
     if ret:
         backupimg = image
-        for i, img in enumerate(image):
-            if cali1.calibrated:
-                backupimg[i] = corn1.image = cali1.undistort(np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY)), cali1.mtx, cali1.dist)
-            else:
-                backupimg[i] = corn1.image = np.float32(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-            keypointsanddescriptors=corn1.updateanddisplay()
-            tomatch[0][i] = keypointsanddescriptors[0]
-            tomatch[1][i] = keypointsanddescriptors[1]
-            cv2.imshow("camera "+str(i), cv2.drawKeypoints(img, keypointsanddescriptors[1], None, color=(0,255,0)))
+        try:
+            for i, img in enumerate(image):
+                if cali1.calibrated:
+                    backupimg[i] = corn1.image = cali1.undistort(np.float32(cv2.cvtColor(image[i], cv2.COLOR_BGR2GRAY)), cali1.mtx, cali1.dist)
+                else:
+                    backupimg[i] = corn1.image = np.float32(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+                tomatch[i] = corn1.updateanddisplay()
+                #cv2.imshow("camera "+str(i), cv2.drawKeypoints(img, tomatch[0][i], None, color=(0,255,0)))
+        except:
+            break
+        framecounter += 1
         image = backupimg
-        print(ret, image)
-        matches = matc1.match(np.uint8(tomatch[0]))[:100]
-        cv2.imshow("matches1", frameshower(cv2.drawMatches(np.uint8(image[0]),tomatch[1][0],np.uint8(image[1]),tomatch[1][1],matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)))
-        cv2.imshow("matches2", frameshower(cv2.drawMatches(np.uint8(image[1]),tomatch[1][0],np.uint8(image[0]),tomatch[1][1],matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)))
+        matches = matc1.match(kps=corn1.corners)[:500]
+        cv2.imshow("matches1", frameshower(cv2.drawMatches(np.uint8(image[0]),tomatch[0],np.uint8(image[1]),tomatch[1],matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)))
+        cv2.imshow("matches2", frameshower(cv2.drawMatches(np.uint8(image[1]),tomatch[0],np.uint8(image[0]),tomatch[1],matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)))
         #cv2.imshow("test", image)
-        point_positions = []
-        #for m in matches:
-         #   locs = [tomatch[0][m.queryIdx], tomatch[0][m.trainIdx]]
-          #  point_positions.append(cali1.calculate_point_relative_position(locs))
+        for m in matches:
+            kps = [tomatch[0][m.queryIdx], tomatch[1][m.trainIdx]]
+            locs = (list(kps[0].pt), list(kps[1].pt))
+            print(locs)
+            point = cali1.calculate_point_relative_position(locs)
+            if point.any():
+                point_positions.append(point)
         key = cv2.waitKey(1)
         if key == ord("c"):
-            cali1.calibrate(cali1.image)
+            cali1.calibrate(image[0])
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
         framestop_time = time.time()
@@ -69,4 +83,7 @@ while cap.isOpened() and cap2.isOpened():
     else:
         print("capture not reading")
         break
+print("CALCULATION FINISHED")
+print("{} points calculated from a total of {} images".format(len(point_positions), framecounter))
+print(point_positions)
 cap.release()
